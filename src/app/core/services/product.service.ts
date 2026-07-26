dea854e55bbc5e4eb96f961b19f533005f242202
= <=>
2026-07-26 14:53:12 -0300

    enviroments

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -2,20 +2,20 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product.interface';
+import { environment } from '../../../environments/environment'; // <--- Importar aquí
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
 export class ProductService {
-	private http = inject(HttpClient);
-	private API_URL = 'http://localhost:8080/api/v1/products';
+  private http = inject(HttpClient);
+  private API_URL = `${environment.apiUrl}/products`;
 
-	getProducts(): Observable<Product[]> {
-		return this.http.get<Product[]>(this.API_URL);
-	}
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.API_URL);
+  }
 
-	// Método para crear un nuevo producto
-	createProduct(product: Partial<Product>): Observable<Product> {
-		return this.http.post<Product>(this.API_URL, product);
-	}
+  createProduct(product: Partial<Product>): Observable<Product> {
+    return this.http.post<Product>(this.API_URL, product);
+  }
 }
