73328c40b890554d880c55d0fc6d654851df8ced
Hugo Javier Rivas Bogado <hugo.rivas.newagesystems@gmail.com>
2026-07-31 12:30:23 -0300

    columnas agregadas a listado de producto

--- src/app/features/admin/products/components/product-table/product-table.component.ts
+++ src/app/features/admin/products/components/product-table/product-table.component.ts
@@ -16,7 +16,11 @@ export class ProductTableComponent implements OnInit {
   @ViewChild(GenericTableComponent) private genericTable!: GenericTableComponent<Product>;
 
   @ViewChild('productCell', { static: true }) productCell!: TemplateRef<any>;
+  @ViewChild('sectionCell', { static: true }) sectionCell!: TemplateRef<any>;
   @ViewChild('categoryCell', { static: true }) categoryCell!: TemplateRef<any>;
+  @ViewChild('subcategoryCell', { static: true }) subcategoryCell!: TemplateRef<any>;
+  @ViewChild('brandCell', { static: true }) brandCell!: TemplateRef<any>;
+  @ViewChild('modelCell', { static: true }) modelCell!: TemplateRef<any>;
   @ViewChild('priceCell', { static: true }) priceCell!: TemplateRef<any>;
   @ViewChild('actionsCell', { static: true }) actionsCell!: TemplateRef<any>;
 
@@ -33,20 +37,27 @@ export class ProductTableComponent implements OnInit {
       { id: 'actions', header: 'Acciones', enableSorting: false, enableColumnFilter: false },
       { accessorKey: 'id', header: 'ID', enableSorting: true, enableColumnFilter: true },
       { accessorKey: 'name', header: 'Producto', enableSorting: true, enableColumnFilter: true },
+      { accessorKey: 'section', header: 'Sección', enableSorting: true, enableColumnFilter: true },
       { accessorKey: 'category', header: 'Categoría', enableSorting: true, enableColumnFilter: true },
+      { accessorKey: 'subcategory', header: 'Subcategoría', enableSorting: true, enableColumnFilter: true },
       { accessorKey: 'brand', header: 'Marca', enableSorting: true, enableColumnFilter: true },
+      { accessorKey: 'model', header: 'Modelo', enableSorting: true, enableColumnFilter: true },
       { accessorKey: 'price', header: 'Precio', enableSorting: true, enableColumnFilter: false }
     ];
 
     this.customTemplates = {
       actions: this.actionsCell,
       name: this.productCell,
+      section: this.sectionCell,
       category: this.categoryCell,
+      subcategory: this.subcategoryCell,
+      brand: this.brandCell,
+      model: this.modelCell,
       price: this.priceCell
     };
   }
 
-  // Método público para que el contenedor pueda recargar la tabla tras guardar/eliminar
+  // Método público para recargar la tabla tras guardar/eliminar
   reload(): void {
     if (this.genericTable) {
       this.genericTable.reload();
